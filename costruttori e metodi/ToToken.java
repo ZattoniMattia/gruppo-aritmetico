fbd7013f3d4bb018e680b708faa6bd86f725814b
maximo-aldrighetti <73687705+maximo-aldrighetti@users.noreply.github.com>
2021-05-20 12:24:07 +0200

    Update ToToken.java

--- costruttori e metodi/ToToken.java
+++ costruttori e metodi/ToToken.java
@@ -1,1 +1,59 @@
-sdbcvkhsdbvdbvsdfkubvguqd
+import java.util.*;
+
+public class ToToken{
+	private String espres;
+	private String[] v_token;
+
+	class ToToken(){
+		this.espress = " ";
+		this.v_token = new String[0];
+
+	}
+	class ToToken(String s){
+		this.espress = s;
+		this v_token =s.split(" ");
+	}
+	public void setToToken(){
+
+	}
+	public void setToToken(String espres, String[] v_token){
+		
+	}
+	public void setEspres(String espres){
+
+	}
+	public String getEspres(){
+    return
+	}
+	public void setV_token(String[] v_token){
+
+	}
+	public String getV_token(){
+    return
+	}
+	public String toString(Totoken t){
+	String t;
+	t=
+	}
+	public boolean equals(ToToken tt){
+    return
+  }
+
+	
+	public int numeroToken(){
+	
+	int cont = this.v_token.lenght();
+	return cont;
+	
+	}
+	public String inverti(String s){
+
+	String inversa = "";
+	for (int i = s.lenght(); i!= 0; i--)
+		for (int y = 0;y<!s.lenght(); y++){
+		inversa.charAt(y) == s.charAt(i);
+
+		}
+	}
+	
+}
