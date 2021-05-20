d1b90a66fe10f44a40abc6fb32cb9bee9fdc615c
VeronesiElisa <73636899+VeronesiElisa@users.noreply.github.com>
2021-05-20 13:04:42 +0200

    Update OperAritm.java

--- OperAritm/OperAritm.java
+++ OperAritm/OperAritm.java
@@ -1,1 +1,46 @@
-public class OperAritm{}
+
+public class OperAritm{
+  int somma;
+  String operatori = "+-*/";
+  
+  public int somma(String [] vett){
+    String[] v_temp = new String(vett);
+    
+    for (int i=0; i<vett.lenght; i++){
+      int ris = 0;
+      switch(vett[i]){
+        case "+" : 
+          ris = vett[i-2].parseInt() + vett[i-1].parseInt();
+          break;
+        case "-" : 
+          ris = vett[i-2].parseInt() - vett[i-1].parseInt();
+          break;
+        case "*" : 
+          ris = vett[i-2].parseInt() * vett[i-1].parseInt();
+          break;
+        case "/" : 
+          ris = vett[i-2].parseInt() / vett[i-1].parseInt();
+          break;
+      }
+      
+     
+    }
+    
+    return somma;
+  }
+  
+ // viene dato in input un vettore, appena trova un operatore mette a null quella cella e quella prima (dove tecnicamente c'è un numero)
+ // mentre quella in posizione i-2 viene sostutuita con il valore ris in forma di Stringa 
+
+ public String remove(String vett, int ris){
+   for(int i=0; i<vett.lenght; i++){
+     if (vett[i] == "+" || vett[i] == "-" || vett[i] == "*" || vett[i] == "/"){
+       vett[i] = null;
+       vett[i-1] = null;
+       String num = String.valueOf(ris);
+       vett[i-2] = num;
+     }
+     
+   }
+ }
+}
